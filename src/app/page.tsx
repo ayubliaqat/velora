cc314ab6adf081b4da6821ad3c0941a75ef332a8
yuvi495 <mayub7540@gmail.com>
2026-04-17 03:22:11 +0500

    feat: setup Tailwind v4 design system using @theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-medium tracking-tight">
+    <main className="min-h-screen flex items-center justify-center bg-base">
+      <h1 className="text-3xl font-medium tracking-tight text-primary">
         VELORA
       </h1>
     </main>
